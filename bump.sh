16b8e4e322aee2b1617ef58a5062477c43860193
Kyryl Melekhin <k.melekhin@gmail.com>
2026-05-16 20:39:32 +0000

    bump.sh: add

---
+++ bump.sh
@@ -0,0 +1,3 @@
+#!/bin/sh -e
+
+EXINIT="g/$1/;;>[ 	]>ya1\:;>[0-9.]+ -\>>;#> >mas\:;'a;'sya2\:;'s;#+1;#>[0-9]>;#>[^0-9.]|\$>ya3:??!p Failed to find $1\:q\!:reg:cd ./%@1:e version:%s/%@2/%@3/g:??w:e sources:%s/%@2/%@3/g:??w\:\!kiss checksum:q" vi -vem dated
