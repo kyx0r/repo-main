ef90a73b7ccadb7c4b9283bb4c8933e986cad89b
Kyryl Melekhin <k.melekhin@gmail.com>
2026-05-16 22:01:38 +0000

    bump.sh: fix

--- bump.sh
+++ bump.sh
@@ -5,5 +5,5 @@ bump() {
 	gal && kcmv
 }
 
-[ -z "$1" ] && exit 0
+[ -z "$1" ] && return 0
 EXINIT="g/$1/;;>[ 	]>ya1\:;>[0-9.]+ -\>>;#> >mas\:;'a;'sya2\:;'s;#+1;#>[0-9]>;#>[^0-9.]|\$>ya3:??!p Failed to find $1\:q\!:reg:cd ./%@1:e version:%s/%@2/%@3/g:??w:e sources:%s/%@2/%@3/g:??w\:\!kiss checksum:q" vi -vem .dated
